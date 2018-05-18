bcf86b1390c14a38ba86871fc241f37f9cbe1650
hathackerwang <deanyuton@gmail.com>
2018-05-18 10:44:14 +0800

    list

---
+++ List_test(v1).java
@@ -0,0 +1,45 @@
+package selection;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class List_test {
+	/*set arr list*/
+	private ArrayList<Course> coursesToSelect;
+	private String inp;
+	private int num;
+	
+	public List_test(){
+		this.coursesToSelect = new ArrayList<Course>();
+		this.inp = getChar();
+		this.num = getId();
+	}
+	
+	public String getChar() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please enter the course:");
+		String s = sc.next();
+		
+		return s;
+	}
+	public int getId() {
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Please enter the id:");
+		int n = sc.nextInt();
+		//sc.close();
+		return n;
+	}
+	
+	public void testAdd() {
+		/*add courses to List*/
+		Course c1 = new Course(this.num, this.inp);
+		coursesToSelect.add(c1);
+		Course temp = coursesToSelect.get(0);
+		
+		System.out.printf("Course %d:%s has been updated", temp.getId()
+				, temp.getName());
+		
+
+	}
+}
