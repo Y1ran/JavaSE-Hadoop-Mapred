025a34ec5e95956125228e1acd21ca133b561186
hathackerwang <deanyuton@gmail.com>
2018-05-22 20:58:31 +0800

    update

---
+++ test_main.java
@@ -0,0 +1,11 @@
+package selection;
+
+public class test_main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		List_test it = new List_test();
+		it.testAdd();
+	}
+
+}
